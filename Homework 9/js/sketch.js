7609cce86222d5d4215c57775c7d437be26a6b78
leerunningcrane <113638868+leerunningcrane@users.noreply.github.com>
2022-12-08 12:55:11 -0700

    adding to hw 9

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -4,47 +4,6 @@ function setup()
 }
 function draw()
 background(120,45,78);
-    textSize(22)
-
-    // head
-    fill(255, 204, 0);
-    circle(250,100,175);
-   
-    // eyes
-    strokeWeight(10);
-    fill(0);
-    point(200,75);
-    point(285,75);
-
-    // nose
-    point(245,90);
-    
-    // mouth
-    ellipse(245, 135, 30, 45)
-
-    // hair
-    line(130,175,175,50);
-    line(325,50,360,175);
-    // body
-    fill(10, 24, 120);
-    rect(200,185,100,150);
-    
-    // decoration
-    fill(255);
-    triangle(220,320,250,220,280,320)
-    // right arm
-    fill(10, 24, 120);
-    rect(300,195,50,10);
-    // left arm
-    rect(150,195,50,10);
-    // left leg
-    rect(200,335,10,50);
-    // right leg
-    rect(290,335,10,50);
-    
-    fill(120);
-    textSize(22);
-    text("Michael Cassens",270,500 );
 {
     background(120,45,78);
     circle(50, 100, 75)
