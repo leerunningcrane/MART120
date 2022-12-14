887c96577b77f6dab279a95f986796a98f0ce45f
leerunningcrane <113638868+leerunningcrane@users.noreply.github.com>
2022-12-14 16:32:20 -0700

    hw 9

---
+++ Homework 9/js/homework_9_2022_12_14_23_29_43/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(128,128,0)
+  
+  fill(0)
+  textSize(22)
+  text("-lee running crane",250,275,175)
+  
+  fill(223,255,0)
+  circle(200,150,175)
+  
+  strokeWeight(10)
+  fill(0)
+  point(150,160)
+  point(250,160)
+  
+  point(200,175)
+  
+  ellipse(200,200,10,20)
+  
+}
--- Homework 9/js/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup()
-{
-    createCanvas(500, 600);
-}
-
-function draw()
-{
-background('rgba(0,255,0, 0.25)');
-}
